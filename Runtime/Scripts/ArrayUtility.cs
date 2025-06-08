2cc64bf0ce6bd61363303d7f75ac270430548de6
Eatjune <490359426@qq.com>
2025-06-08 12:11:35 +0800

    修改名空间

--- Runtime/Scripts/ArrayUtility.cs
+++ Runtime/Scripts/ArrayUtility.cs
@@ -1,29 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using Unity.Mathematics;
-using UnityEngine;
 
-namespace GamePlay {
+namespace LymeGame.Util {
 	public static class ArrayUtility {
 		/// <summary>
-		/// 数组的2个元素位置调换
+		///     数组的2个元素位置调换
 		/// </summary>
 		public static void Swap<T>(this T[] array, int index1, int index2) {
 			(array[index2], array[index1]) = (array[index1], array[index2]);
 		}
 
 		/// <summary>
-		/// 列表的2个元素位置调换
+		///     列表的2个元素位置调换
 		/// </summary>
 		public static void Swap<T>(this List<T> list, int index1, int index2) {
 			(list[index2], list[index1]) = (list[index1], list[index2]);
 		}
 
 		/// <summary>
-		/// 乱序排序数组
+		///     乱序排序数组
 		/// </summary>
 		public static T[] SortRandom<T>(this T[] array) {
-			var random = new System.Random();
+			var random = new Random();
 			return array.OrderBy(n => random.Next()).ToArray();
 			// for (var i = array.Length - 1; i > 0; i--) {
 			// 	var randomIndex = Random.Range(0, i);
@@ -32,10 +31,10 @@ namespace GamePlay {
 		}
 
 		/// <summary>
-		/// 乱序排序列表
+		///     乱序排序列表
 		/// </summary>
 		public static List<T> SortRandom<T>(this List<T> list) {
-			var random = new System.Random();
+			var random = new Random();
 			return list.OrderBy(n => random.Next()).ToList();
 			// for (var i = list.Count - 1; i > 0; i--) {
 			// 	var randomIndex = Random.Range(0, i);
